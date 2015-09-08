bcd0bb7589cd2489ed7b8b3b208943d3a5eab1c3
Craig <atk.craig@gmail.com>
2015-09-08 11:38:55 -0500

    Adding scaffolded create form

---
+++ SharpStandup/SharpStandup/Controllers/ReportController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SharpStandup.Models;
+
+namespace SharpStandup.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateReportResponse response)
+        {
+            return View();
+        }
+    }
+}
---
+++ SharpStandup/SharpStandup/Models/CreateReportResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SharpStandup.Models
+{
+    public class CreateReportResponse
+    {
+        public string Yesterday { get; set; }
+
+        [Required(ErrorMessage="Please enter what you plan to work on today")]
+        public string Today { get; set; }
+
+        public string Impediments { get; set; }
+    }
+}
